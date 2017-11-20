e4770773e1ee0098598bb99d09840cc6101f6686
Natty Davis <natty.davis79@gmail.com>
2017-11-20 08:59:43 -0500

    set up react router for / path

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//react router modules
+import {Route, BrowserRouter} from 'react-router-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path='/' exact component={App}/>
+    </div>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
